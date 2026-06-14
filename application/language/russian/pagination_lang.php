7d245219f1b11e9bb6f5a8e0ec2ee0ed7a762447
elrafir <elrafir@gmail.com>
2026-06-14 22:17:59 +0300

    Исправление: Добавлен отсутствующий языковой файл pagination_lang.php для русской локализации

---
+++ application/language/russian/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['pagination_first_link'] = '« Первая';
+$lang['pagination_next_link'] = '›';
+$lang['pagination_prev_link'] = '‹';
+$lang['pagination_last_link'] = 'Последняя »';
